1a2ae7d27613bbcd62cf9bfd8dbf3f014a591704
DerPercy <christian.chsc.schmidt@gmail.com>
2023-09-14 19:44:45 +0200

    Sorare ranking stuff

--- m_code/sorare/client.py
+++ m_code/sorare/client.py
@@ -49,7 +49,27 @@ class Client:
         print(self.jwt)
         pass
 
-    def request(self,body:str, variables = {}):
+    def request(self,body:str, variables = {},options = {}):
+        int_result = self.__request(body,variables,options)
+        result = int_result.get("result")
+        
+        # pagination
+        if options.get("pagination") != None:
+            p_options = options.get("pagination")
+            while len(result) < p_options.get("targetNumber"):
+                cursor = json_selector(int_result.get("full_result"),p_options.get("cursorSelector"))
+                #print(cursor)
+                variables[p_options.get("paginationVariable")] = cursor
+                int_result = self.__request(body,variables,options)
+                result.extend(int_result.get("result"))
+        #print(json.dumps(r.json(),indent=2))
+        return result
+
+    def __request(self,body:str, variables = {},options = {}):
+        """ Returns an object with the following keys
+        - result: the result
+        - full_result: the complete result (in case f.e. options.resultSelector was set)
+        """
         headers = {
             'content-type': 'application/json', 
             'Accept-Charset': 'UTF-8',
@@ -62,5 +82,21 @@ class Client:
 		}
         r = requests.post("https://api.sorare.com/graphql", json=payloadJSON, headers=headers)
         print(r.status_code)
-        #print(json.dumps(r.json(),indent=2))
-        return r.json()
+
+        result = r.json()
+        
+
+        # preselect result
+        if options.get("resultSelector") != None:
+            result = json_selector(result,options.get("resultSelector"))
+        return {
+            "result": result,
+            "full_result": r.json()
+        }
+            
+
+def json_selector(json_obj, selector_array):
+    for selector in selector_array:
+        #print(selector)
+        json_obj = json_obj.get(selector)
+    return json_obj
--- m_code/sorare/rankings.py
+++ m_code/sorare/rankings.py
@@ -1,6 +1,43 @@
 from .client import Client
 from .models.ranking import Ranking
 
+import json
 
 def get_rankings_of_leader_board_slug(client:Client,leader_board_slug:str,number: int) -> list[Ranking]:
+    body = """
+query RankingsQuery($leaderBoardSlug: String!, $afterCursor: String) {
+  football {
+    so5 {
+      so5Leaderboard( slug: $leaderBoardSlug ) {
+        so5Rankings( first: 50 after: $afterCursor) {
+          pageInfo {
+            endCursor hasNextPage hasPreviousPage startCursor 
+          }
+          nodes {
+            ranking
+            so5Lineup {
+              user {
+                id
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+"""
+    variables = {
+        "leaderBoardSlug": leader_board_slug
+    }
+    options = {
+        "resultSelector": ["data","football","so5","so5Leaderboard","so5Rankings","nodes"],
+        "pagination": {
+            "targetNumber": number,
+            "paginationVariable": "afterCursor",
+            "cursorSelector": ["data","football","so5","so5Leaderboard","so5Rankings","pageInfo","endCursor"]
+        }
+    }
+    result= client.request(body,variables,options)
+    print(len(result))
     pass
